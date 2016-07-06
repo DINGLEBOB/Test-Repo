28a44bf11988506022fe789b4c330575adbfb41b
Jake Marohl <Jake Marohl>
2016-07-06 10:32:51 -0600

    Added in eslint wdio.conf

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "global-require": [
+            "error",
+        ],
+    }
+};
